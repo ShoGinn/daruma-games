ad58045169e506de59133ba541012d86f6993d6e
Scott <2660603+ShoGinn@users.noreply.github.com>
2022-12-13 19:25:43 -0500

    refactor: cleanup migrations

--- src/migrations/Migration20221214000607.ts
+++ src/migrations/Migration20221214000607.ts
@@ -1,13 +1,11 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20221214000607 extends Migration {
+    async up(): Promise<void> {
+        this.addSql('alter table `user` add `karma_shop` json null;');
+    }
 
-  async up(): Promise<void> {
-    this.addSql('alter table `user` add `karma_shop` json null;');
-  }
-
-  async down(): Promise<void> {
-    this.addSql('alter table `user` drop `karma_shop`;');
-  }
-
+    override async down(): Promise<void> {
+        this.addSql('alter table `user` drop `karma_shop`;');
+    }
 }
