f1b1ecb4cd26a37c533ad6daa7a9f2d25bd046cf
Scott <2660603+ShoGinn@users.noreply.github.com>
2022-12-30 21:43:14 -0500

    refactor: clean up migration order

--- src/services/Database.ts
+++ src/services/Database.ts
@@ -6,17 +6,20 @@ const initializeMikroOrm = async (): Promise<MikroORM> => {
     const orm = await MikroORM.init(config);
     const migrator = orm.getMigrator();
     // create migration if no one is present in the migrations folder
-    const pendingMigrations = await migrator.getPendingMigrations();
+    let pendingMigrations = await migrator.getPendingMigrations();
     const executedMigrations = await migrator.getExecutedMigrations();
+
+    if (pendingMigrations.length === 0 && executedMigrations.length === 0) {
+        await migrator.createInitialMigration();
+    } else {
+        await migrator.createMigration();
+        pendingMigrations = await migrator.getPendingMigrations();
+    }
     // migrate to the latest migration
     if (pendingMigrations?.length > 0) {
         await migrator.up();
     }
 
-    if (pendingMigrations.length === 0 && executedMigrations.length === 0) {
-        await migrator.createInitialMigration();
-    }
-    await migrator.createMigration();
     return orm;
 };
 export default initializeMikroOrm;
