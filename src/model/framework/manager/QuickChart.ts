aad2ff89b43febfd1cc316b5f499ea2d54bf757a
Scott <2660603+ShoGinn@users.noreply.github.com>
2023-02-02 21:22:50 -0500

    feat: add charts for daruma holders

--- src/model/framework/manager/QuickChart.ts
+++ src/model/framework/manager/QuickChart.ts
@@ -1,5 +1,4 @@
 import { AbstractRequestEngine } from '../engine/impl/AbstractRequestEngine.js';
-
 export class QuickChartRequestEngine extends AbstractRequestEngine {
     constructor() {
         super('https://quickchart.io');
@@ -8,5 +7,18 @@ export class QuickChartRequestEngine extends AbstractRequestEngine {
     public async getChart(params: Record<string, any>): Promise<any> {
         const response = await this.api.get('/chart', { params });
         return response.data;
+    }
+    public async generateChartImage(chartOptions: Record<string, any>): Promise<any> {
+        // append format=png to the chart options to get a PNG image
+        chartOptions.format = 'png';
+        const response = await this.api.get('/chart', {
+            params: { c: JSON.stringify(chartOptions) },
+        });
+        return response.data;
+    }
+    public generateChartURL(params: Record<string, any>): string {
+        return `${this.baseUrl}/chart?bkg=%23ffffff&c=${encodeURIComponent(
+            JSON.stringify(params)
+        )}`;
     }
 }
