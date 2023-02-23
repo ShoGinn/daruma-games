b1c34ba36c5493c112791b0cf6cb2cf64440d758
Scott <2660603+ShoGinn@users.noreply.github.com>
2023-02-22 20:49:47 -0500

    refactor: add faker

--- tests/utils/testFuncs.ts
+++ tests/utils/testFuncs.ts
@@ -1,5 +1,5 @@
+import { faker } from '@faker-js/faker';
 import { EntityManager } from '@mikro-orm/core';
-import { randomBytes } from 'node:crypto';
 
 import { AlgoNFTAsset } from '../../src/entities/AlgoNFTAsset.entity.js';
 import { AlgoStdAsset } from '../../src/entities/AlgoStdAsset.entity.js';
@@ -10,24 +10,16 @@ interface CreateAssetFunc {
     creatorWallet: AlgoWallet;
     asset: AlgoNFTAsset;
 }
-export function generateRandomString(length: number): string {
-    const bytes = randomBytes(Math.ceil(length / 2));
-    return bytes.toString('hex').slice(0, length);
-}
 export async function createRandomAsset(db: EntityManager): Promise<CreateAssetFunc> {
-    const randomName = generateRandomString(10);
-    const randomUnitName = generateRandomString(10);
-    // create a random url that looks like a url
-    const randomUrl = `https://${generateRandomString(10)}.com`;
     const creatorUser = await createRandomUser(db);
     const creatorWallet = await createRandomWallet(creatorUser, db);
-    const assetIndex = Math.floor(Math.random() * 100000);
+
     const asset = new AlgoNFTAsset(
-        assetIndex,
+        faker.datatype.number({ min: 1_000_000_000 }),
         creatorWallet,
-        randomName,
-        randomUnitName,
-        randomUrl
+        faker.name.firstName(),
+        faker.name.lastName(),
+        faker.internet.url()
     );
     await db.getRepository(AlgoNFTAsset).persistAndFlush(asset);
     return { creatorUser, creatorWallet, asset };
@@ -37,25 +29,25 @@ export async function createRandomUser(
     db: EntityManager,
     userName?: string | undefined
 ): Promise<User> {
-    const userId = userName ?? generateRandomString(10);
+    const userId = userName ?? faker.random.alphaNumeric(10);
     const user = new User(userId);
     await db.getRepository(User).persistAndFlush(user);
     return user;
 }
 export async function createRandomWallet(user: User, db: EntityManager): Promise<AlgoWallet> {
-    const walletAddress = generateRandomString(10);
+    const walletAddress = faker.finance.bitcoinAddress();
     const wallet = new AlgoWallet(walletAddress, user);
     await db.getRepository(AlgoWallet).persistAndFlush(wallet);
     return wallet;
 }
 
 export async function createRandomASA(db: EntityManager): Promise<AlgoStdAsset> {
-    const randomName = generateRandomString(10);
-    const randomUnitName = generateRandomString(10);
-    // create a random url that looks like a url
-    const randomUrl = `https://${generateRandomString(10)}.com`;
-    const assetIndex = Math.floor(Math.random() * 100000);
-    const asset = new AlgoStdAsset(assetIndex, randomName, randomUnitName, randomUrl);
+    const asset = new AlgoStdAsset(
+        faker.datatype.number({ min: 1_000_000_000 }),
+        faker.name.firstName(),
+        faker.name.lastName(),
+        faker.internet.url()
+    );
     await db.getRepository(AlgoStdAsset).persistAndFlush(asset);
     return asset;
 }
