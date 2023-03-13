bc030ff6a78e117886dcad1a555586b4c5846872
Scott <2660603+ShoGinn@users.noreply.github.com>
2023-03-13 16:54:29 -0400

    build: add initial dtBoard testing

--- src/utils/classes/dtBoard.ts
+++ src/utils/classes/dtBoard.ts
@@ -10,41 +10,76 @@ export class DarumaTrainingBoard {
     ROUND_WIDTH = 20;
     ATTACK_ROW_SPACER = '\t';
     ROUND_AND_TOTAL_SPACER = '\t';
-    /**
-     * Create a row of total damage with blank spaces factored in
-     *
-    
-     * @param {number} rollIndex
-     * @param {number} roundIndex
-     * @param {number} playerIndex
-     * @param {Player[]} players
-     * @param {RenderPhases} renderPhase
-     * @returns {*}  {string}
-     */
-    public renderBoard(
-        rollIndex: number,
-        roundIndex: number,
-        playerIndex: number,
-        players: Array<Player>,
-        renderPhase: RenderPhases
-        // isLastRender: boolean
-    ): string {
-        const board = [];
-        const blankRow = ' '.repeat(this.ROUND_WIDTH);
-        const horizontalRule = `~~${blankRow}${this.ROUND_AND_TOTAL_SPACER}${blankRow}~~`;
-        // create a row representing the current round
-        board.push(`>>> ${this.centerString(horizontalRule.length - 4, '***ROUND***')}\n`);
-        board.push(`${this.createRoundNumberRow(roundIndex)}\n`);
-        // create a row of blank spaces double roundWidth
-        board.push(horizontalRule);
-        // create a row displaying attack numbers for each player
-        // as well as a row displaying the total
-        board.push(
-            this.createAttackAndTotalRows(players, playerIndex, rollIndex, roundIndex, renderPhase)
-        );
 
-        return board.join('\n');
+    /**
+     * Centers a string within the given amount of space
+     * using the delimiter to fill the space to both the left & right of the string
+     *
+     * @param {number} space - the desired amount of space that the string should be centered within.
+     * @param {string} [content=''] - the string to be centered.
+     * @param {string} [delimiter=' '] - the character used to fill the space on either side of the string.
+     * @returns {*}  {string}
+     * @memberof DarumaTrainingBoard
+     */
+    centerString(space: number, content: string = '', delimiter: string = ' '): string {
+        const len = content.length;
+        const padSpace = Math.floor((space - len) / 2);
+        return content.padStart(len + padSpace, delimiter).padEnd(space, delimiter);
     }
+
+    /**
+     * Gets the Image based upon the phase of the game
+     *
+     * @param {RollData} roll
+     * @param {boolean} isPrevRoll
+     * @param {boolean} isCurrentRoll
+     * @param {boolean} isTurnRoll
+     * @param {RenderPhases} renderPhase
+     * @param {boolean} hasBeenTurn
+     * @memberof DarumaTrainingBoard
+     * @returns {string}
+     */
+    getImageType = (
+        roll: RollData,
+        isPrevRoll: boolean,
+        isCurrentRoll: boolean,
+        isTurnRoll: boolean,
+        renderPhase: RenderPhases,
+        hasBeenTurn: boolean
+    ): string => {
+        const emoji = 'ph';
+
+        // if it's a previous roll, just show png
+        if (isPrevRoll) {
+            return `${roll.damage}png`;
+        }
+        // if it's the current players roll and we're in gif render phase add gif
+        if (isCurrentRoll && isTurnRoll) {
+            if (renderPhase === RenderPhases.GIF) {
+                return `roll`;
+            } else if (renderPhase === RenderPhases.EMOJI) {
+                return `${roll.damage}png`;
+            }
+        } else if (isCurrentRoll && !isTurnRoll) {
+            return hasBeenTurn ? `${roll.damage}png` : 'ph';
+        }
+
+        return emoji;
+    };
+    /**
+     * Creates a row of attack numbers for each player
+     *
+     * @param {(number | string)} [roundNum]
+     * @memberof DarumaTrainingBoard
+     * @returns {string}
+     */
+    createRoundCell = (roundNum?: number | string): string => {
+        let stringNum = roundNum || ' ';
+        if (typeof stringNum === 'number') {
+            stringNum = stringNum.toString();
+        }
+        return this.centerString(this.ROUND_WIDTH, stringNum);
+    };
 
     /**
      * Creates a row of attack numbers for each player
@@ -75,77 +110,6 @@ export class DarumaTrainingBoard {
         roundNumberRow.splice(1, 0, this.ROUND_AND_TOTAL_SPACER);
         return roundNumberRow.join('');
     }
-
-    /**
-     * Creates a row of attack numbers for each player
-     *
-     * @param {(number | string)} [roundNum]
-     * @memberof DarumaTrainingBoard
-     * @returns {string}
-     */
-    createRoundCell = (roundNum?: number | string): string => {
-        let stringNum = roundNum || ' ';
-        if (typeof stringNum === 'number') {
-            stringNum = stringNum.toString();
-        }
-        return this.centerString(this.ROUND_WIDTH, stringNum);
-    };
-
-    /**
-     * Creates a row of attack numbers for each player
-     *
-     * @param {Array<Player>} players
-     * @param {number} playerIndex
-     * @param {number} rollIndex
-     * @param {number} roundIndex
-     * @param {RenderPhases} renderPhase
-     * @memberof DarumaTrainingBoard
-     * @returns {string}
-     */
-    createAttackAndTotalRows = (
-        players: Array<Player>,
-        playerIndex: number,
-        rollIndex: number,
-        roundIndex: number,
-        renderPhase: RenderPhases
-    ): string => {
-        const rows: Array<string> = [];
-        // For each player
-        players.map((player: Player, index: number) => {
-            const { rounds } = player.roundsData;
-
-            // check if it is or has been players turn yet to determine if we should show the attack roll
-            const isTurn = index === playerIndex;
-            const hasBeenTurn = index < playerIndex;
-            const notTurnYet = index > playerIndex;
-
-            const attackRow = this.createAttackRow(
-                rounds,
-                roundIndex,
-                rollIndex,
-                isTurn,
-                renderPhase,
-                hasBeenTurn
-            );
-            attackRow.splice(1, 0, this.ATTACK_ROW_SPACER);
-            rows.push(attackRow.join(''));
-
-            // add round total row
-            const totalRow = this.createTotalRow(
-                roundIndex,
-                rollIndex,
-                rounds,
-                renderPhase,
-                hasBeenTurn,
-                notTurnYet
-            );
-            totalRow.splice(1, 0, this.ROUND_AND_TOTAL_SPACER);
-            rows.push(`__${totalRow.join('')}__`);
-        });
-
-        return rows.join('\n');
-    };
-
     /**
      * Creates a row of attack numbers for each player
      *
@@ -221,34 +185,6 @@ export class DarumaTrainingBoard {
         return row;
     };
 
-    getImageType = (
-        roll: RollData,
-        isPrevRoll: boolean,
-        isCurrentRoll: boolean,
-        isTurnRoll: boolean,
-        renderPhase: RenderPhases,
-        hasBeenTurn: boolean
-    ): string => {
-        const emoji = 'ph';
-
-        // if it's a previous roll, just show png
-        if (isPrevRoll) {
-            return `${roll.damage}png`;
-        }
-        // if it's the current players roll and we're in gif render phase add gif
-        if (isCurrentRoll && isTurnRoll) {
-            if (renderPhase === RenderPhases.GIF) {
-                return `roll`;
-            } else if (renderPhase === RenderPhases.EMOJI) {
-                return `${roll.damage}png`;
-            }
-        } else if (isCurrentRoll && !isTurnRoll) {
-            return hasBeenTurn ? `${roll.damage}png` : 'ph';
-        }
-
-        return emoji;
-    };
-
     /**
      * Creates a row of total numbers for each player
      *
@@ -298,19 +234,95 @@ export class DarumaTrainingBoard {
 
         return totalRowLabel;
     };
+
     /**
-     * Centers a string within the given amount of space
-     * using the delimiter to fill the space to both the left & right of the string
+     * Create a row of total damage with blank spaces factored in
      *
-     * @param {number} space - the desired amount of space that the string should be centered within.
-     * @param {string} [content=''] - the string to be centered.
-     * @param {string} [delimiter=' '] - the character used to fill the space on either side of the string.
+    
+     * @param {number} rollIndex
+     * @param {number} roundIndex
+     * @param {number} playerIndex
+     * @param {Player[]} players
+     * @param {RenderPhases} renderPhase
      * @returns {*}  {string}
-     * @memberof DarumaTrainingBoard
      */
-    centerString(space: number, content: string = '', delimiter: string = ' '): string {
-        const len = content.length;
-        const padSpace = Math.floor((space - len) / 2);
-        return content.padStart(len + padSpace, delimiter).padEnd(space, delimiter);
+    public renderBoard(
+        rollIndex: number,
+        roundIndex: number,
+        playerIndex: number,
+        players: Array<Player>,
+        renderPhase: RenderPhases
+        // isLastRender: boolean
+    ): string {
+        const board = [];
+        const blankRow = ' '.repeat(this.ROUND_WIDTH);
+        const horizontalRule = `~~${blankRow}${this.ROUND_AND_TOTAL_SPACER}${blankRow}~~`;
+        // create a row representing the current round
+        board.push(`>>> ${this.centerString(horizontalRule.length - 4, '***ROUND***')}\n`);
+        board.push(`${this.createRoundNumberRow(roundIndex)}\n`);
+        // create a row of blank spaces double roundWidth
+        board.push(horizontalRule);
+        // create a row displaying attack numbers for each player
+        // as well as a row displaying the total
+        board.push(
+            this.createAttackAndTotalRows(players, playerIndex, rollIndex, roundIndex, renderPhase)
+        );
+
+        return board.join('\n');
     }
+
+    /**
+     * Creates a row of attack numbers for each player
+     *
+     * @param {Array<Player>} players
+     * @param {number} playerIndex
+     * @param {number} rollIndex
+     * @param {number} roundIndex
+     * @param {RenderPhases} renderPhase
+     * @memberof DarumaTrainingBoard
+     * @returns {string}
+     */
+    createAttackAndTotalRows = (
+        players: Array<Player>,
+        playerIndex: number,
+        rollIndex: number,
+        roundIndex: number,
+        renderPhase: RenderPhases
+    ): string => {
+        const rows: Array<string> = [];
+        // For each player
+        players.map((player: Player, index: number) => {
+            const { rounds } = player.roundsData;
+
+            // check if it is or has been players turn yet to determine if we should show the attack roll
+            const isTurn = index === playerIndex;
+            const hasBeenTurn = index < playerIndex;
+            const notTurnYet = index > playerIndex;
+
+            const attackRow = this.createAttackRow(
+                rounds,
+                roundIndex,
+                rollIndex,
+                isTurn,
+                renderPhase,
+                hasBeenTurn
+            );
+            attackRow.splice(1, 0, this.ATTACK_ROW_SPACER);
+            rows.push(attackRow.join(''));
+
+            // add round total row
+            const totalRow = this.createTotalRow(
+                roundIndex,
+                rollIndex,
+                rounds,
+                renderPhase,
+                hasBeenTurn,
+                notTurnYet
+            );
+            totalRow.splice(1, 0, this.ROUND_AND_TOTAL_SPACER);
+            rows.push(`__${totalRow.join('')}__`);
+        });
+
+        return rows.join('\n');
+    };
 }
--- tests/tests/dtBoard.spec.ts
+++ tests/tests/dtBoard.spec.ts
@@ -3,59 +3,72 @@ import { describe, expect, it } from '@jest/globals';
 
 import { RenderPhases } from '../../src/enums/dtEnums.js';
 import { DarumaTrainingBoard } from '../../src/utils/classes/dtBoard.js';
+describe('DarumaTrainingBoard', () => {
+    let board: DarumaTrainingBoard;
+    beforeAll(() => {
+        // create the DarumaTrainingBoard object
+        board = new DarumaTrainingBoard();
+    });
+    describe('centerString', () => {
+        it('returns the content centered within the given space using the delimiter to fill the space on either side of the string', () => {
+            const content = 'Hello World';
+            const delimiter = ' ';
+            const result = board.centerString(board.ROUND_WIDTH, content, delimiter);
+            expect(result).toBe('    Hello World     ');
+            expect(result).toHaveLength(board.ROUND_WIDTH);
+        });
+    });
+    describe('getImageType', () => {
+        // Test 1 - Get image type for previous roll
+        it('should return `roll_damage.png` for a previous roll', () => {
+            const roll = { damage: 5 } as RollData;
+            const result = board.getImageType(roll, true, false, false, RenderPhases.GIF, false);
+            expect(result).toEqual(`${roll.damage}png`);
+        });
 
-let board: DarumaTrainingBoard;
-beforeAll(() => {
-    // create the DarumaTrainingBoard object
-    board = new DarumaTrainingBoard();
-});
-describe('createRoundNumberRow', () => {
-    it('First round creates expected string', () => {
-        const roundNumberRow = board.createRoundNumberRow(0);
-        expect(roundNumberRow).toContain('       :one:        	                    ');
-    });
+        // Test 2 - Get image type for current roll and turn roll
+        it('should return `roll` for a current roll in gif render phase', () => {
+            const roll = { damage: 5 } as RollData;
+            const result = board.getImageType(roll, false, true, true, RenderPhases.GIF, false);
+            expect(result).toEqual('roll');
+        });
 
-    it('Non-first round creates expected string', () => {
-        const roundNumberRow = board.createRoundNumberRow(1);
-        expect(roundNumberRow).toContain('       :one:        	       :two:        ');
+        // Test 3 - Get image type for current roll and not turn roll
+        it('should return `ph` for a current roll that is not turn roll', () => {
+            const roll = { damage: 5 } as RollData;
+            const result = board.getImageType(roll, false, true, false, RenderPhases.EMOJI, false);
+            expect(result).toEqual(`ph`);
+        });
     });
-    it('Non-first round creates expected string', () => {
-        const roundNumberRow = board.createRoundNumberRow(2);
-        expect(roundNumberRow).toContain('       :two:        	      :three:       ');
+    describe('createRoundCell', () => {
+        it('should return a string with the round number centered in the cell equal to the ROUND_WIDTH in length', () => {
+            const roundNumber = 1;
+            const result = board.createRoundCell(roundNumber);
+            expect(result).toContain('         1          ');
+            expect(result).toHaveLength(board.ROUND_WIDTH);
+        });
     });
-    it('Non-first round creates expected string', () => {
-        const roundNumberRow = board.createRoundNumberRow(3);
-        expect(roundNumberRow).toContain('      :three:       	       :four:       ');
-    });
-});
-describe('getImageType', () => {
-    // Test 1 - Get image type for previous roll
-    it('should return `roll_damage.png` for a previous roll', () => {
-        const roll = { damage: 5 } as RollData;
-        const result = board.getImageType(roll, true, false, false, RenderPhases.GIF, false);
-        expect(result).toEqual(`${roll.damage}png`);
-    });
+    describe('createRoundNumberRow', () => {
+        it('First round creates expected string', () => {
+            const roundNumberRow = board.createRoundNumberRow(0);
+            expect(roundNumberRow).toContain('       :one:        	                    ');
+            expect(roundNumberRow).toHaveLength(board.ROUND_WIDTH * 2 + 1);
+        });
 
-    // Test 2 - Get image type for current roll and turn roll
-    it('should return `roll` for a current roll in gif render phase', () => {
-        const roll = { damage: 5 } as RollData;
-        const result = board.getImageType(roll, false, true, true, RenderPhases.GIF, false);
-        expect(result).toEqual('roll');
-    });
-
-    // Test 3 - Get image type for current roll and not turn roll
-    it('should return `ph` for a current roll that is not turn roll', () => {
-        const roll = { damage: 5 } as RollData;
-        const result = board.getImageType(roll, false, true, false, RenderPhases.EMOJI, false);
-        expect(result).toEqual(`ph`);
-    });
-});
-describe('centerString', () => {
-    it('returns the content centered within the given space using the delimiter to fill the space on either side of the string', () => {
-        const content = 'Hello World';
-        const space = 20;
-        const delimiter = ' ';
-        const result = board.centerString(space, content, delimiter);
-        expect(result).toBe('    Hello World     ');
+        it('Non-first round creates expected string', () => {
+            const roundNumberRow = board.createRoundNumberRow(1);
+            expect(roundNumberRow).toContain('       :one:        	       :two:        ');
+            expect(roundNumberRow).toHaveLength(board.ROUND_WIDTH * 2 + 1);
+        });
+        it('Non-first round creates expected string', () => {
+            const roundNumberRow = board.createRoundNumberRow(2);
+            expect(roundNumberRow).toContain('       :two:        	      :three:       ');
+            expect(roundNumberRow).toHaveLength(board.ROUND_WIDTH * 2 + 1);
+        });
+        it('Non-first round creates expected string', () => {
+            const roundNumberRow = board.createRoundNumberRow(3);
+            expect(roundNumberRow).toContain('      :three:       	       :four:       ');
+            expect(roundNumberRow).toHaveLength(board.ROUND_WIDTH * 2 + 1);
+        });
     });
 });
