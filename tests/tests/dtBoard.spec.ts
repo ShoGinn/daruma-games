ccf15826638b79b446b0f76222434d8e1637841a
Scott <2660603+ShoGinn@users.noreply.github.com>
2023-03-14 17:43:43 -0400

    refactor: start the dtBoard updates

--- tests/tests/dtBoard.spec.ts
+++ tests/tests/dtBoard.spec.ts
@@ -17,6 +17,38 @@ describe('DarumaTrainingBoard', () => {
             expect(result).toBe('    Hello World     ');
             expect(result).toHaveLength(board.ROUND_WIDTH);
         });
+        it('returns the content centered when no content or delimiter is given', () => {
+            const result = board.centerString(board.ROUND_WIDTH);
+            expect(result).toBe('                    ');
+            expect(result).toHaveLength(board.ROUND_WIDTH);
+        });
+        it('returns the single digit content centered within the given space using the delimiter to fill the space on either side of the string', () => {
+            const content = '1';
+            const delimiter = ' ';
+            const result = board.centerString(board.ROUND_WIDTH, content, delimiter);
+            expect(result).toBe('         1          ');
+            expect(result).toHaveLength(board.ROUND_WIDTH);
+        });
+        it('returns the 2 digits content centered within the given space using the delimiter to fill the space on either side of the string', () => {
+            const content = '12';
+            const delimiter = ' ';
+            const result = board.centerString(board.ROUND_WIDTH, content, delimiter);
+            expect(result).toBe('         12         ');
+            expect(result).toHaveLength(board.ROUND_WIDTH);
+        });
+
+        it.skip('should take an discord type emoji string and return the content centered extending the given space to accommodate the length of the discord emoji using the delimiter to fill the space on either side of the string', () => {
+            const content = ':one:';
+            const delimiter = ' ';
+            const result = board.centerString(board.ROUND_WIDTH, content, delimiter);
+            expect(result).toBe('         :one:          ');
+        });
+        it.skip('should take 2 discord type emojis and return the content centered extending the given space to accommodate the length of the discord emoji using the delimiter to fill the space on either side of the string', () => {
+            const content = ':one::two:';
+            const delimiter = ' ';
+            const result = board.centerString(board.ROUND_WIDTH, content, delimiter);
+            expect(result).toBe('         :one::two:         ');
+        });
     });
     describe('getImageType', () => {
         // Test 1 - Get image type for previous roll
