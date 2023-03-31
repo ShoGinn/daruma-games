0b7852fcf391cb608aa0c077f86c51a7d4e36ec5
Scott <2660603+ShoGinn@users.noreply.github.com>
2023-03-31 10:29:49 -0400

    refactor: clean up code and update deps

--- src/model/framework/decorators/discord-error-handler.ts
+++ src/model/framework/decorators/discord-error-handler.ts
@@ -1,7 +1,15 @@
-import { DiscordAPIError, RESTJSONErrorCodes } from 'discord.js';
+import { DiscordAPIError, RESTJSONErrorCodes as DiscordApiErrors } from 'discord.js';
 
 import logger from '../../../utils/functions/logger-factory.js';
-
+const IGNORED_ERRORS = new Set([
+    DiscordApiErrors.UnknownMessage,
+    DiscordApiErrors.UnknownChannel,
+    DiscordApiErrors.UnknownGuild,
+    DiscordApiErrors.UnknownMember,
+    DiscordApiErrors.UnknownUser,
+    DiscordApiErrors.UnknownInteraction,
+    DiscordApiErrors.MissingAccess,
+]);
 export function withCustomDiscordApiErrorLogger(
     target: unknown,
     propertyKey: string,
@@ -12,16 +20,15 @@ export function withCustomDiscordApiErrorLogger(
         try {
             return originalMethod.apply(this, originalArguments);
         } catch (error) {
-            if (error instanceof DiscordAPIError) {
-                if (
-                    error.code === RESTJSONErrorCodes.UnknownInteraction ||
-                    error.code === RESTJSONErrorCodes.UnknownMessage
-                ) {
-                    logger.debug('Unknown Interaction or Message');
-                } else {
-                    // if the error is DiscordAPIError[10062]: Unknown interaction skip it otherwise log it
-                    logger.error(error);
-                }
+            if (
+                error instanceof DiscordAPIError &&
+                typeof error.code == 'number' &&
+                IGNORED_ERRORS.has(error.code)
+            ) {
+                logger.debug('Unknown Interaction or Message');
+            } else {
+                // if the error is DiscordAPIError[10062]: Unknown interaction skip it otherwise log it
+                logger.error(error);
             }
         }
     };
--- tests/decortators/discord-error-handler.spec.ts
+++ tests/decortators/discord-error-handler.spec.ts
@@ -3,6 +3,18 @@ import { DiscordAPIError, RESTJSONErrorCodes } from 'discord.js';
 
 import { withCustomDiscordApiErrorLogger } from '../../src/model/framework/decorators/discord-error-handler.js';
 import logger from '../../src/utils/functions/logger-factory.js';
+
+function subFunction(): void {
+    throw new DiscordAPIError(
+        { code: RESTJSONErrorCodes.UnknownInteraction, message: 'Test Debug' },
+        RESTJSONErrorCodes.UnknownInteraction,
+        500,
+        'GET',
+        '/test',
+        {}
+    );
+}
+
 describe('withErrorHandling decorator', () => {
     let targetFunction: any;
     let loggerErrorSpy: jest.SpyInstance<void, [any]>;
@@ -17,6 +29,7 @@ describe('withErrorHandling decorator', () => {
     afterEach(() => {
         // Clear the logger spy after each test
         loggerErrorSpy.mockClear();
+        loggerDebugSpy.mockClear();
     });
 
     describe('when an error that is not an interaction or message error is thrown', () => {
@@ -75,6 +88,30 @@ describe('withErrorHandling decorator', () => {
         it('should call the logger function', () => {
             // Call the decorated method and expect the logger function to be called with the error
             targetFunction();
+            expect(loggerErrorSpy).toHaveBeenCalledTimes(0);
+            expect(loggerDebugSpy).toHaveBeenCalledTimes(1);
+            expect(loggerDebugSpy).toHaveBeenCalledWith('Unknown Interaction or Message');
+        });
+    });
+    describe('when a message or interaction error is thrown in a sub function', () => {
+        beforeEach(() => {
+            // Create a test class and method with the decorator
+            class TestClass {
+                @withCustomDiscordApiErrorLogger
+                testMethod(): void {
+                    subFunction();
+                }
+            }
+
+            // Instantiate the test class and get a reference to the decorated method
+            const testObject = new TestClass();
+            targetFunction = testObject.testMethod.bind(testObject);
+        });
+
+        it('should call the logger function', () => {
+            // Call the decorated method and expect the logger function to be called with the error
+            targetFunction();
+            expect(loggerErrorSpy).toHaveBeenCalledTimes(0);
             expect(loggerDebugSpy).toHaveBeenCalledTimes(1);
             expect(loggerDebugSpy).toHaveBeenCalledWith('Unknown Interaction or Message');
         });
