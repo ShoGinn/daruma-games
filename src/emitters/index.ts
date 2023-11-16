52b1ec33694782a6f94fe458c48f193daa8aec98
Scott <2660603+ShoGinn@users.noreply.github.com>
2023-11-16 17:52:24 -0500

    refactor: clean up embedmanager tests

--- src/emitters/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './global-emitter.js';
-export * from './types.js';
--- tests/tests/dt-embedmanager.spec.ts
+++ tests/tests/dt-embedmanager.spec.ts
@@ -4,17 +4,15 @@ import { Message } from 'discord.js';
 import { anything, instance, mock, verify, when } from 'ts-mockito';
 
 // import { DarumaTrainingGameRepository } from '../../src/repositories/dt-game-repository.js';
-import { GameStatus } from '../../src/enums/daruma-training.js';
+import { GameStatus, GameTypes } from '../../src/enums/daruma-training.js';
+import { MaintenanceService } from '../../src/services/maintenance.js';
 import { EmbedManager } from '../../src/utils/classes/dt-embedmanager.js';
+import { GameState } from '../../src/utils/classes/dt-game-state.js';
 import { Game } from '../../src/utils/classes/dt-game.js';
 import { WaitingRoomManager } from '../../src/utils/classes/dt-waitingroommanager.js';
 import { doEmbed, postGameWinEmbeds } from '../../src/utils/functions/dt-embeds.js';
-import { isInMaintenance } from '../../src/utils/functions/maintenance.js';
+import { mockFakeGame } from '../utils/fake-mocks.js';
 
-const isInMaintenanceMock = isInMaintenance as jest.MockedFunction<typeof isInMaintenance>;
-jest.mock('../../src/utils/functions/maintenance.js', () => ({
-  isInMaintenance: jest.fn(),
-}));
 jest.mock('../../src/utils/functions/dt-embeds.js', () => ({
   doEmbed: jest.fn(),
   postGameWinEmbeds: jest.fn(),
@@ -22,36 +20,26 @@ jest.mock('../../src/utils/functions/dt-embeds.js', () => ({
 describe('EmbedManager', () => {
   let embedManager: EmbedManager;
   let game: Game;
-  let mockedGame: Game;
   let waitingRoomManager: WaitingRoomManager;
-  let mockedWaitingRoomManager: WaitingRoomManager;
+  let mockMaintenanceService: MaintenanceService;
+  let gameState: GameState;
 
   beforeEach(() => {
     // Arrange
-    embedManager = new EmbedManager();
-    game = mock(Game);
-    mockedGame = instance(game);
+    mockMaintenanceService = mock(MaintenanceService);
+    embedManager = new EmbedManager(instance(mockMaintenanceService));
+    game = mock(instance(Game));
     waitingRoomManager = mock(WaitingRoomManager);
-    mockedWaitingRoomManager = instance(waitingRoomManager);
-    when(game.waitingRoomManager).thenReturn(mockedWaitingRoomManager);
+    gameState = new GameState(mockFakeGame(GameTypes.OneVsNpc).settings.token, undefined);
+
+    when(game.waitingRoomManager).thenReturn(instance(waitingRoomManager));
     when(waitingRoomManager.sendToChannel(anything())).thenResolve({} as unknown as Message);
-    mockedGame.settings = { maxCapacity: 1 } as unknown as Game['settings'];
-    mockedGame.state = {
-      status: GameStatus.waitingRoom,
-      finishGame: jest.fn(),
-      maintenance: jest.fn(),
-      reset: jest.fn(),
-      canStartGame: jest.fn().mockReturnValue(true),
-    } as unknown as Game['state'];
-    mockedGame.state.finishGame = jest
+    when(game.state).thenReturn(gameState);
+    gameState.reset = jest.fn().mockReturnValue({ ...gameState, status: GameStatus.waitingRoom });
+    gameState.finishGame = jest.fn().mockReturnValue({ ...gameState, status: GameStatus.win });
+    gameState.maintenance = jest
       .fn()
-      .mockReturnValue({ ...mockedGame.state, status: GameStatus.win });
-    mockedGame.state.reset = jest
-      .fn()
-      .mockReturnValue({ ...mockedGame.state, status: GameStatus.waitingRoom });
-    mockedGame.state.maintenance = jest
-      .fn()
-      .mockReturnValue({ ...mockedGame.state, status: GameStatus.maintenance });
+      .mockReturnValue({ ...gameState, status: GameStatus.maintenance });
   });
   afterEach(() => {
     jest.clearAllMocks();
@@ -68,7 +56,7 @@ describe('EmbedManager', () => {
     test('should generate a mock embed and attempt to send it', async () => {
       // Arrange
       // Act
-      const result = await embedManager['sendEmbed'](mockedGame);
+      const result = await embedManager['sendEmbed'](instance(game));
       // Assert
       expect(result).toBeDefined();
       verify(waitingRoomManager.sendToChannel(anything())).once();
@@ -80,7 +68,7 @@ describe('EmbedManager', () => {
       // Arrange
       const message = { edit: jest.fn() } as unknown as Message;
       // Act
-      await embedManager['updateMessage'](mockedGame, message);
+      await embedManager['updateMessage'](instance(game), message);
       // Assert
       expect(message.edit).toHaveBeenCalledTimes(1);
       expect(doEmbed).toHaveBeenCalledTimes(1);
@@ -89,7 +77,7 @@ describe('EmbedManager', () => {
       // Arrange
       const message = { edit: jest.fn(() => Promise.reject()) } as unknown as Message;
       // Act
-      await embedManager['updateMessage'](mockedGame, message);
+      await embedManager['updateMessage'](instance(game), message);
       // Assert
       expect(message.edit).toHaveBeenCalledTimes(1);
       expect(doEmbed).toHaveBeenCalledTimes(1);
@@ -99,7 +87,7 @@ describe('EmbedManager', () => {
     test('should send the win embeds', async () => {
       // Arrange
       // Act
-      await embedManager['sendWinEmbeds'](mockedGame);
+      await embedManager['sendWinEmbeds'](instance(game));
       // Assert
       verify(waitingRoomManager.sendToChannel(anything())).once();
       expect(postGameWinEmbeds).toHaveBeenCalledTimes(1);
@@ -109,7 +97,7 @@ describe('EmbedManager', () => {
     test('should delete the waiting room message and send the game embed', async () => {
       // Arrange
       // Act
-      await embedManager.startGame(mockedGame);
+      await embedManager.startGame(instance(game));
       // Assert
       expect(embedManager.activeGameEmbed).toBeDefined();
     });
@@ -119,12 +107,11 @@ describe('EmbedManager', () => {
       // Arrange
       embedManager.activeGameEmbed = { edit: jest.fn() } as unknown as Message;
       // Act
-      await embedManager.finishGame(mockedGame);
+      await embedManager.finishGame(instance(game));
       // Assert
       expect(embedManager.activeGameEmbed).toBeUndefined();
       expect(postGameWinEmbeds).toHaveBeenCalledTimes(1);
       verify(waitingRoomManager.sendToChannel(anything())).twice();
-      expect(isInMaintenanceMock).toHaveBeenCalledTimes(1);
       expect(embedManager.waitingRoomEmbed).toBeDefined();
       expect(embedManager.gameBoardMessage).toBeUndefined();
     });
@@ -132,12 +119,11 @@ describe('EmbedManager', () => {
       // Arrange
       embedManager.activeGameEmbed = undefined;
       // Act
-      await embedManager.finishGame(mockedGame);
+      await embedManager.finishGame(instance(game));
       // Assert
       expect(embedManager.activeGameEmbed).toBeUndefined();
       expect(postGameWinEmbeds).toHaveBeenCalledTimes(1);
       verify(waitingRoomManager.sendToChannel(anything())).once();
-      expect(isInMaintenanceMock).toHaveBeenCalledTimes(0);
       expect(embedManager.waitingRoomEmbed).toBeUndefined();
       expect(embedManager.gameBoardMessage).toBeUndefined();
     });
@@ -146,7 +132,7 @@ describe('EmbedManager', () => {
     test('should send the game board message', async () => {
       // Arrange
       // Act
-      await embedManager.executeGameBoardMessage(mockedGame, 'test');
+      await embedManager.executeGameBoardMessage(instance(game), 'test');
       // Assert
       expect(embedManager.gameBoardMessage).toBeDefined();
     });
@@ -154,7 +140,7 @@ describe('EmbedManager', () => {
       // Arrange
       embedManager.gameBoardMessage = { edit: jest.fn() } as unknown as Message;
       // Act
-      await embedManager.executeGameBoardMessage(mockedGame, 'test');
+      await embedManager.executeGameBoardMessage(instance(game), 'test');
       // Assert
       expect(embedManager.gameBoardMessage).toBeDefined();
     });
@@ -163,23 +149,21 @@ describe('EmbedManager', () => {
     test('should reset the game state, reset the embed manager, and send the join waiting room embed', async () => {
       // Arrange
       // Act
-      await embedManager.sendJoinWaitingRoomEmbed(mockedGame);
+      await embedManager.sendJoinWaitingRoomEmbed(instance(game));
       // Assert
       expect(embedManager.waitingRoomEmbed).toBeDefined();
       expect(embedManager.gameBoardMessage).toBeUndefined();
-      expect(mockedGame.state).toBeDefined();
-      expect(mockedGame.state.status).toEqual(GameStatus.waitingRoom);
-      expect(isInMaintenanceMock).toHaveBeenCalledTimes(1);
+      expect(game.state).toBeDefined();
+      expect(gameState.reset).toHaveBeenCalledTimes(1);
     });
     test('should not send the join waiting room embed if the game is in maintenance', async () => {
       // Arrange
-      isInMaintenanceMock.mockResolvedValue(true);
+      when(mockMaintenanceService.isInMaintenance()).thenResolve(true);
       // Act
-      await embedManager.sendJoinWaitingRoomEmbed(mockedGame);
+      await embedManager.sendJoinWaitingRoomEmbed(instance(game));
       // Assert
       expect(embedManager.waitingRoomEmbed).toBeDefined();
       expect(embedManager.gameBoardMessage).toBeUndefined();
-      expect(isInMaintenanceMock).toHaveBeenCalledTimes(1);
     });
   });
   describe('updateWaitingRoomEmbed', () => {
@@ -187,7 +171,7 @@ describe('EmbedManager', () => {
       // Arrange
       embedManager.waitingRoomEmbed = { edit: jest.fn() } as unknown as Message;
       // Act
-      await embedManager.updateWaitingRoomEmbed(mockedGame);
+      await embedManager.updateWaitingRoomEmbed(instance(game));
       // Assert
       expect(embedManager.waitingRoomEmbed).toBeDefined();
     });
@@ -195,7 +179,7 @@ describe('EmbedManager', () => {
       // Arrange
       embedManager.waitingRoomEmbed = undefined;
       // Act
-      await embedManager.updateWaitingRoomEmbed(mockedGame);
+      await embedManager.updateWaitingRoomEmbed(instance(game));
       // Assert
       expect(embedManager.waitingRoomEmbed).toBeUndefined();
     });
