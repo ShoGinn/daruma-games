8315688f7eaff8c68bb42a1c566f5acd88942b93
Scott <2660603+ShoGinn@users.noreply.github.com>
2023-11-18 18:30:37 -0500

    refactor: add mongo memory server

--- tests/setup/jest-mitm.ts
+++ tests/setup/jest-mitm.ts
@@ -3,6 +3,10 @@ import Mitm from 'mitm';
 const mitm = Mitm();
 
 mitm.on('connect', (socket, options) => {
+  if (options.host === '127.0.0.1') {
+    socket.bypass();
+    return;
+  }
   // eslint-disable-next-line no-console
   console.log(
     `Blocked network connection to ${options.host?.toString() ?? 'Unknown'}:${options.port}`,
